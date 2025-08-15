f52384b2009f69ede2c3b9e95ce4bc681be9941e
Phil Belinsky <philbelinsky@yahoo.com>
2025-08-15 10:50:29 -0400

    booking confirmation email added

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -1,5 +1,6 @@
 import stripe from "stripe";
 import Booking from "../models/Booking.js";
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks = async (req, res) => {
     const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
@@ -26,7 +27,10 @@ export const stripeWebhooks = async (req, res) => {
                 const {bookingId} = session.metadata
             
                 await Booking.findByIdAndUpdate(bookingId, {isPaid: true, paymentLink:""});
-                
+                await inngest.send({
+                    name: "app/show.booked",
+                    data: {bookingId}
+                })
                 break;
             }
             default:
